1b8658d920c07b22ea8cbbc4ebbfe77e960b4a83
thexeromin <thexeromin@gmail.com>
2024-05-31 11:40:17 +0530

    add: frontend & update: response to json

--- flaskr/shorten.py
+++ flaskr/shorten.py
@@ -2,39 +2,45 @@ import functools
 import uuid
 
 from flask import (
-    Blueprint, flash, g, request, redirect
+    Blueprint, flash, g, request, redirect, render_template, jsonify
 )
 from flaskr.db import get_db
 
 bp = Blueprint('shorten', __name__)
 
 
-@bp.route('/', methods=['POST'])
+@bp.route('/', methods=('GET', 'POST'))
 def shorten_url():
-    url = request.form['url']
-    key = str(uuid.uuid4())[:4]
-    db = get_db()
-    error = None
+    if request.method == 'POST':
+        url = request.form['url']
+        key = str(uuid.uuid4())[:4]
+        db = get_db()
+        error = None
 
-    if not url:
-        error = 'url is required.'
-    
-    if error is None:
-        try:
-            db.execute(
-                "INSERT INTO data (url, key) VALUES (?, ?)",
-                (url, key),
-            )
-            db.commit()
-        except db.IntegrityError:
-            error = f"{url} is already exists."
-        else:
-            return f"{request.url_root}{key}"
-    
-    flash(error)
+        if not url:
+            error = 'url is required.'
+        
+        if error is None:
+            try:
+                db.execute(
+                    "INSERT INTO data (url, key) VALUES (?, ?)",
+                    (url, key),
+                )
+                db.commit()
+            except db.IntegrityError:
+                error = f"{url} is already exists."
+            else:
+                res = {
+                    'shortUrl': f"{request.url_root}{key}"
+                }
+                return jsonify(res)
+        
+        flash(error)
+
+    return render_template('index.html')
 
 
-@bp.route('/<key>', methods=['GET'])
+@bp.route('/<key>', methods=('GET', 'POST'))
 def get_url(key):
     db = get_db()
     error = None
